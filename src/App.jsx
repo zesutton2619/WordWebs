d24716ef319f41022f68ba759e4b8e2b857de1e8
Zach <zesutton2619@eagle.fgcu.edu>
2025-08-09 11:07:06 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,44 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { DiscordProvider } from "./components/DiscordProvider";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import WordWebs from "./components/WordWebs";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 import TermsOfService from "./components/TermsOfService";
+import { DiscordProvider } from "./components/DiscordProvider";
 
 function App() {
   return (
-    <DiscordProvider>
-      <Router>
-        <div className="min-h-screen bg-slate-900 text-white flex flex-col">
-          <div className="flex-1">
-            <Routes>
-              <Route path="/" element={<WordWebs />} />
-              <Route path="/privacy" element={<PrivacyPolicy />} />
-              <Route path="/terms" element={<TermsOfService />} />
-            </Routes>
-          </div>
+    <div className="min-h-screen bg-slate-900 text-white flex flex-col">
+      <BrowserRouter basename="/">
+        <div className="flex-1">
+          <Routes>
+            {/* Game Route */}
+            <Route
+              path="/"
+              element={
+                <DiscordProvider>
+                  <WordWebs />
+                </DiscordProvider>
+              }
+            />
 
-          {/* Footer with links */}
-          <footer className="bg-slate-800 text-slate-400 text-sm p-4 text-center">
-            <Link to="/privacy" className="hover:text-white mx-2">
-              Privacy Policy
-            </Link>
-            |
-            <Link to="/terms" className="hover:text-white mx-2">
-              Terms of Service
-            </Link>
-          </footer>
+            {/* Static Legal Pages */}
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<TermsOfService />} />
+          </Routes>
         </div>
-      </Router>
-    </DiscordProvider>
+
+        {/* Footer with links */}
+        <footer className="bg-slate-800 text-slate-400 text-sm p-4 text-center">
+          <Link to="/privacy" className="hover:text-white mx-2">
+            Privacy Policy
+          </Link>
+          |
+          <Link to="/terms" className="hover:text-white mx-2">
+            Terms of Service
+          </Link>
+        </footer>
+      </BrowserRouter>
+    </div>
   );
 }
 
